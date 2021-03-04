701c6fac37c976e56302ed951ba1e94f1b2723fe
YeaJi1984 <79080662+YeaJi1984@users.noreply.github.com>
2021-03-04 17:52:12 +0900

    Add files via upload

---
+++ lecture05.js
@@ -0,0 +1,21 @@
+// var a = 100, b = 3.14;
+// var c = "안녕하세요", d ="a";
+// var e = true, f = false;
+
+
+// console.log(a, typeof(a));
+// console.log(c, typeof(c));
+// console.log(e, typeof(e));
+
+
+
+
+var n =1;
+console.log();
+
+var b = true;
+console.log();
+
+var s = "문자열입니다"
+
+
